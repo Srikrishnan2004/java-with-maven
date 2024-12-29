5ef55ead723bb1648f9e9b3288354874b933bd16
Srikrishnan2004 <velayuthamsrikrishnan@gmail.com>
2024-12-29 18:36:51 +0530

    first commit

---
+++ src/test/java/com/example/TestWithGA/TestWithGaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestWithGA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWithGaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
